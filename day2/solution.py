7de93c4472eb51c599e81de4a311965afb73944a
diogofernandesc <diogofernandescon@gmail.com>
2021-12-02 19:47:58 +0000

    day2

---
+++ day2/solution.py
@@ -0,0 +1,45 @@
+def part1():
+    horizontal, depth = 0, 0
+    depth_moves = {
+        "down": lambda x: int(x),
+        "up": lambda x: -(int(x))
+    }
+
+    horizontal_moves = {
+        "forward": lambda x: int(x),
+    }
+
+    with open("input.txt") as f:
+        for command in f:
+            move, scalar = command.split(" ")
+            depth += depth_moves.get(move, lambda x: 0)(scalar)
+            horizontal += horizontal_moves.get(move, lambda x: 0)(scalar)
+
+    return horizontal * depth
+
+
+def part2():
+    horizontal, depth, aim = 0, 0, 0
+    depth_moves = {
+        "down": lambda x: int(x),
+        "up": lambda x: -(int(x))
+    }
+
+    horizontal_moves = {
+        "forward": lambda x: int(x),
+    }
+    with open("input.txt") as f:
+        for command in f:
+            move, scalar = command.split(" ")
+            move_horizontally = horizontal_moves.get(move, lambda x: 0)
+            move_vertically = depth_moves.get(move, lambda x: 0)
+
+            aim += move_vertically(scalar)
+            depth += aim * move_horizontally(scalar)
+            horizontal += move_horizontally(scalar)
+
+    return horizontal * depth
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
